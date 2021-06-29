89b017b298050a73fd478f2c2a16ef972da5a900
takuma375 <takuma-shimizu-10174@keio.jp>
2021-06-29 21:41:58 +0900

    Update main()内部の処理を追加
    入力、確認、終了のそれぞれの機能を実現するため、モードを受け取りswitch文で分岐させた
    その他の処理は、別の関数をそれぞれ呼び出し、必要に応じてaccountbook.go内のメソッドを活用した。

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"os"
 )
 
 // accountbook.goを用いて処理を分割する
@@ -10,6 +11,44 @@ func main() {
 
 	// NewAccountBookを使用して、Accountbookを作成する
 	ab := NewAccountBook("accountbook.txt")
+
+	// 以下のループにラベルを付ける
+LOOP:
+	for {
+		// モードを選択して実行できるようにする
+		var mode int
+		fmt.Println("[1]入力 [2]最新10件 [3]終了")
+		fmt.Print("> ")
+		fmt.Scan(&mode)
+
+		// modeによって処理を変える
+		switch mode {
+		case 1: // 入力
+			// データの入力件数を受け取る
+			var n int
+			fmt.Print("何件入力しますか? ")
+			fmt.Scan(&n)
+
+			for i := 0; i <= n; i++ {
+				err := ab.AddItem(inputItem())
+				if err != nil {
+					fmt.Fprintln(os.Stderr, "エラー", err)
+					// LOOPという名前のついたforから抜け出す
+					break LOOP
+				}
+			}
+		case 2: //最新10件
+			items, err := ab.GetItems(10)
+			if err != nil {
+				fmt.Fprintln(os.Stderr, "エラー", err)
+				break LOOP
+			}
+			showItems(items)
+		case 3: // 終了
+			fmt.Println("終了します")
+			os.Exit(0)
+		}
+	}
 
 }
 
