a4d574049e83174e6c9bf5ddbaf5e9fa6046bc80
takuma375 <takuma-shimizu-10174@keio.jp>
2021-06-29 19:52:41 +0900

    Add GetItems()メソッドを追加。
    最近追加したものを指定した最大件数分だけ取得するメソッドを定義した。
    エラーが発生した場合は、第2戻り値で返すように処理。
    
    データのパースを行う部分に関しては別途定義する。

--- accountbook.go
+++ accountbook.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"log"
 	"os"
@@ -34,4 +35,41 @@ func (ab *Accountbook) AddItem(item *Item) error {
 
 	// 成功終了したことを伝えるため、nilを返す
 	return nil
+}
+
+// 最近追加したものを最大limit件だけItemを取得するメソッドを定義する。もしエラーが発生したら第2戻り値で返す
+func (ab *Accountbook) GetItems(limit int) ([]*Item, error) {
+
+	// 読み込み専用でファイルを開く
+	file, err := os.Open(ab.fileName)
+	if err != nil {
+		return nil, err
+	}
+
+	// 関数終了時にファイルを閉じる
+	defer file.Close()
+
+	// ファイルからデータを読み込む
+	scanner := bufio.NewScanner(file)
+
+	// 戻り値を保管するための変数を定義
+	var items []*Item
+
+	// 読み込んだファイルを1行ずつ処理する
+	for scanner.Scan() {
+		// TODO: データのパースを行う関数を別途定義
+	}
+
+	// スキャナーでエラーが発生していないか確認
+	if err := scanner.Err(); err != nil {
+		return nil, err
+	}
+
+	// limit件よりも少ない場合は、全件返す
+	if len(items) < limit {
+		return items, nil
+	}
+
+	// limit件よりも多い場合は、itemsの後方limit件だけ返す
+	return items[len(items)-limit : len(items) : len(items)], nil
 }
